6914167fe06d85c000507d0bfd9265ceb5286a97
sitikfatikhah <khamidatul94wnsb@gmail.com>
2026-07-28 22:39:40 +0700

    migration setup v.0.0.2

--- database/migrations/2026_06_28_013410_create_settings_table.php
+++ database/migrations/2026_06_28_013410_create_settings_table.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->string('key');
+
+            $table->string('key', 100);
+
             $table->json('value')->nullable();
-            $table->string('type')->default('string');
-            $table->string('group')->nullable();
-            $table->string('scope')->default('global');
-            $table->string('role')->nullable();
-            $table->string('permission')->nullable();
+
+            $table->string('type', 50)->default('string');
+            $table->string('group', 100)->nullable();
+            $table->string('scope', 50)->default('global');
+            $table->string('role', 100)->nullable();
+            $table->string('permission', 100)->nullable();
+
             $table->boolean('is_active')->default(true);
+
             $table->timestamps();
+
             $table->unique(
-            ['key', 'group', 'scope', 'role', 'permission'],
-            'scope_unique'
-        );
+                ['key', 'group', 'scope', 'role', 'permission'],
+                'scope_unique'
+            );
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('settings');
