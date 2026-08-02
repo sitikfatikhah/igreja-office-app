ce0d6781c4e61bd1138b181cbcc314d66e78b201
sitikfatikhah <khamidatul94wnsb@gmail.com>
2026-08-02 22:17:53 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/face-enroll.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/face-enrollment.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
